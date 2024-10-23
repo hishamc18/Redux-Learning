fa4637f43600247bc40358998b2daef5ba1494ca
Hisham C <hishamc18@gmail.com>
2024-10-23 18:41:23 +0530

    middleware example for redux-xore by mimicing a delay for a counter increment fn()

--- middlewareexample/src/redux/store.jsx
+++ middlewareexample/src/redux/store.jsx
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware } from "redux";
 import { thunk } from 'redux-thunk';
 
 
+
+//middleware example with mimicing a delay for counter
 export const incrementAsync = (delay) => {
     return (dispatch) => {
         setTimeout(() => {
