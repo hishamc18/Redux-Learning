c1b954b5e7dedf67dabcbc7451b65f8cb05f8a63
Hisham C <hishamc18@gmail.com>
2024-10-23 18:36:19 +0530

    createAsyncThunk example

--- create-async-thunk-example/src/features/UserSlice.jsx
+++ create-async-thunk-example/src/features/UserSlice.jsx
@@ -15,7 +15,7 @@ export const userSlice = createSlice({
     },
     reducers: {},
     extraReducers: (builder) => {
-        builder
+        builder   //builder is afn provided b building pattern of createSlice, which do the work of a switch
         .addCase(fetchUsers.pending, (state) => {
             state.status = 'loading'
         })
